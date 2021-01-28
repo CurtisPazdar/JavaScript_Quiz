64ca4b9b737824d194891422d7049b4e246abf6b
Curtis P <curtis.pazdar@gmail.com>
2021-01-27 19:22:09 -0700

    Semantic HTML & Comments. Working Code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # JavaScript_Quiz
+
+This is a quiz that will begin when you press on the button that first appears.
+
+This quiz will then begin at 45 seconds and count down while you go over multiple choice questions.
+
+If the correct answer is selected for the multiple choice question the next question with choices are rendered. If not the timer has 15 seconds subtracted from it.
+
+If the time runs out or the player reaches the maximum score the game is over.
+
+When the game is over a series of prompts appear stating the game is over, the users score, and asking for initials to be stored alongside the score.
+
+The score is logged to local memory along with the players initials and the page reloads to start the quiz anew.
--- script.js
+++ script.js
@@ -15,11 +15,13 @@ var secondsLeft = 45; //creating the countdown start for the timer. Set to 45 se
 
 var timer = document.querySelector("#timerArea");//declaring the variable timer to be related to the id timerArea
 
+//declaring the button variables to be related to the button ids on the html.
 var button1 = document.querySelector("#button1")
 var button2 = document.querySelector("#button2")
 var button3 = document.querySelector("#button3")
 var button4 = document.querySelector("#button4")
 
+//Create an array to house questions and answers on buttons. Set the correct answer in the correct category so we can match against it later.
 var questions = [
     { questionArea : "What command do you use to add an element to the end of an array?",
     button1 : "unshift",
@@ -68,28 +70,32 @@ var questions = [
     button4 : "NetscapeDev",
     correct : "button3"
     },{
+        
         questionArea : "GAME OVER - CONGRATULATIONS" 
     }
     
 
 ];
-//this will be the last question 
-var lastquestion = questions.length-1;
-// this function reacts on click and lets the people
 
+//when the element with the startButton id is clicked run the function StartQuiz
 document.getElementById("startButton").onclick = function startQuiz(){
 
     //get startButton and sets its display to none  to hide. Access the quiz_box and make its style block so as to unhide it. 
     document.getElementById("startButton").style.display = "none";
     document.getElementById("quiz_box").style.display = "block";
     
-    
+    //putting a variable name to the number 0 for ease of use later.
     var zero = 0;
 
-// function that allows the questions to have the text content of the current state where zero is in the array. Zero = 0 , Zero++ = 1
+    // function that allows the questions to have the text content of the current state where zero is in the array. Zero = 0 , Zero++ = 1
     function createQs () {
 
+    //The variable is equal to the current array values for the array value equal to the variable zero. This begins on 0 and increases by one each time a correct answer is clicked.
+
     var render = questions[zero]
+
+
+    //Now that the elements inside quiz box are set to display = block we can now set there text values. Their values are equal to the current array being selected using the zero variable and its corresponding properties listed below.
 
     header.textContent = "JAVASCRIPT QUIZ";
     scoreArea.textContent = 0;
@@ -102,23 +108,36 @@ document.getElementById("startButton").onclick = function startQuiz(){
 
     }
 
+    //We now add an add event listener to the buttons contained in the div answers.
     answers.addEventListener("click", function(event){
+        //I want the event.target to be a variable named answer check
         var answerCheck = event.target;
+        // we are creating a local variable associated with the id displayMessage
         var displayMessage = document.querySelector("#displayMessage")
-
+        //This if statement states that if the current array property correct's string is the same as the button clicked assigned id then it is truthee. If not it is false.
         if(questions[zero].correct === answerCheck.id){
+            //In the text area display correct if truthee
             displayMessage.innerHTML = "CORRECT";
+            //Increase the zero variable by 1
             zero++;
+            // now run our function again to render the new set of questions.
             createQs();
+            //Increase the score area with the current position in the array. Which happens to be the same as the score.
             scoreArea.innerHTML = zero;
 
         } else {
+            //If the answer is not correct display the text WRONG and reduce 15 seconds from the timer.
             displayMessage.innerHTML = "WRONG";
             secondsLeft = secondsLeft-15;
             
         };
 
+    
         var Scores = localStorage.getItem("Score")
+
+
+        //If seconds on the timer are below zero or if the score has reached 7. Game Over. Turn the quiz display off and begin a series of alerts and prompts. Then log the score and the initials captured in the prompt to the local storage. Lastly reload the page so the user could start again.
+
 
         if (secondsLeft <= 0 || zero >= 7 ){
             quiz_box.style.display = "none";
@@ -138,17 +157,14 @@ document.getElementById("startButton").onclick = function startQuiz(){
 
    
 
-
+    //function to set the timer and its action of reducing incrementally and reducing every 1000 millisecond.
     function setTimer() {
         // Sets interval in variable
         var timerInterval = setInterval(function() {
           secondsLeft--;
           timer.textContent = secondsLeft;
           if(secondsLeft === 0) {
-            // Stops execution of action at set interval
             clearInterval(timerInterval);
-            // Calls function to create and append image
-            sendMessage();
           }
       
         }, 1000);}
@@ -156,11 +172,11 @@ document.getElementById("startButton").onclick = function startQuiz(){
 
 
     
-    
+    //run our two functions for the first time. This is the only time set time is run. This is not the only time createQs() is run. 
     
     setTimer();
     createQs();
-    creatAlerts();
+    
     
 
 }
